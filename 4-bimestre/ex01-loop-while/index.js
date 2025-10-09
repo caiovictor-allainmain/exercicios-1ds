a0bb569e71b47a22991e72d14f6f55c6b22d388c
Caio Victor Barros Uchoa <caio.uchoa1@aluno.ce.gov.br>
2025-10-09 17:40:01 +0000

    Resolvido!

--- 4-bimestre/ex01-loop-while/index.js
+++ 4-bimestre/ex01-loop-while/index.js
@@ -1,4 +1,10 @@
 // CRIE UMA LÓGICA AQUI
+let resultado = 1
 
+while (resultado < 10) {
+    console.log(`deu certo ${resultado}!!`)
+
+resultado = resultado + 1
+}
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
