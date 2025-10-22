34e092551d08390b7df5e20996650823418be071
Caio Victor Barros Uchoa <caio.uchoa1@aluno.ce.gov.br>
2025-10-22 18:40:35 +0000

    concluido

--- 4-bimestre/projetos/projeto5-sistema-de-controle-de-producao/index.js
+++ 4-bimestre/projetos/projeto5-sistema-de-controle-de-producao/index.js
@@ -2,3 +2,36 @@
 // Comando: npm i prompt-sync; npm i -D jest
 
 // CRIE UMA LÓGICA ABAIXO
+const prompt = require(`prompt-sync`)()
+
+let dia = 1
+let continuarProducao 
+let total = 0
+let resumo = ""
+
+do{
+    let num = parseInt(prompt("Quantas peças devem ser produzidas? "))
+    let pecas = 0
+
+    while (pecas < num) {
+        pecas++
+        console.log(`Peça ${pecas} produzida com sucesso.`)
+    }
+
+    total += pecas
+   
+    if(pecas == num){
+        console.log("Meta alcançada!")
+    } else {
+        console.log("Meta não alcançada.")
+    }
+   
+    console.log(`Total Geral:${total}`)
+    resumo += ` Resumo dos dias anteriores:
+    Dia ${dia}: ${pecas} peças produzida(S) com sucesso \n`
+    dia++
+    continuarProducao = prompt("Deseja simular outro turno? (S/N): ").toUpperCase()
+}while(continuarProducao == "S")
+console.log(resumo)
+console.log(`Total geral:${total}`)
+console.log("Encerrando sistema de produção...")
